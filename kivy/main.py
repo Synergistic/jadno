b063077d43f7bf0c730bc484407041d62339b357
Synergistic <DFernandez226@gmail.com>
2014-01-20 17:32:37 -0500

    Working card images!
    Now draws all the cards and can be dragged around the screen

---
+++ kivy/main.py
@@ -0,0 +1,57 @@
+
+
+from glob import glob
+from random import randint
+from os.path import join, dirname
+from kivy.app import App
+from kivy.logger import Logger
+from kivy.uix.scatter import Scatter
+from kivy.properties import StringProperty
+from kivy.config import Config
+
+from uno import *
+
+
+
+Config.set( 'graphics', 'width', '1280' )
+Config.set( 'graphics', 'height', '720' )
+
+class Cardimg(Scatter):
+    '''Picture is the class that will show the image with a white border and a
+    shadow. They are nothing here because almost everything is inside the
+    picture.kv. Check the rule named <Picture> inside the file, and you'll see
+    how the Picture() is really constructed and used.
+
+    The source property will be the filename to show.
+    '''
+
+    source = StringProperty(None)
+
+
+class JadnoApp(App):
+
+    def build(self):
+
+        # the root is created in pictures.kv
+        root = self.root
+
+        # get any files into images directory
+        # curdir = dirname(__file__)
+		
+        for card in my_deck.deck_list:
+            try:
+                # load the image
+                picture = Cardimg(source=card.image)
+                # add to the main field
+                root.add_widget(picture)
+            except Exception, e:
+                Logger.exception('Pictures: Unable to load <%s>' % filename)
+
+    def on_pause(self):
+        return True
+
+my_deck = Deck(make_cards())
+my_deck.shuffle()
+if __name__ == '__main__':
+    JadnoApp().run()
+
--- kivy/uno.py
+++ kivy/uno.py
@@ -2,125 +2,65 @@
 import os
 import random
 
-RANKS = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 's', 'r', 'd2', 'wd4', 'w' ]
-COLORS = [ 'R', 'G', 'B', 'Y' ]
+RANKS = [ '0', '1', '1', '2', '2', '3', '3', '4', '4', '5', '5', 
+        '6', '6', '7', '7', '8', '8', '9', '9', 's', 's', 'r', 'r', 
+		'd2', 'd2', 'wd4', 'w' ]
+		
+COLORS = [ 'b', 'g', 'r', 'y' ]
 
 class Card( object ):
-	''' A Card object that represents UNO cards.
-	It  has a rank; 0-9, skip, reverse, draw two,
-	wild, wild draw four. As well as a color; blue, green, red
-	yellow'''
+    ''' A Card object that represents UNO cards.
+    It  has a rank; 0-9, skip, reverse, draw two,
+    wild, wild draw four. As well as a color; blue, green, red
+    yellow'''
 
 
-	def __init__( self, color, rank):
-		self.rank = rank
-		self.color = color
+    def __init__( self, color, rank, image ):
+	    self.rank = rank
+	    self.color = color
+	    self.image = image
 
-	def __str__( self ):
-		if self.color == None:
-		  return str( self.rank )
-		  
-		else:
-		  card_rep = self.color + str( self.rank )
-		  return card_rep
+    def __str__( self ):
+        if self.color == None:
+            return str( self.rank )
+	  
+        else:
+            card_rep = self.color + str( self.rank )
+            return card_rep
 
-class Hand( object ):
-	'''a Hand is made up of 1+ Uno cards. Cards
-	can be added, or removed (i.e. playing a card)
-	'''
-
-	def __init__( self ):
-		self.cards = []
-		self.uno = False
-		
-	def __str__( self ):
-		hand_rep = " "
-		for card in self.cards:
-			hand_rep += str( card ) + ' '
-		return hand_rep
-
-	def add_card( self, card ):
-		self.cards.append( card )
-
-
-	def discard_card( self, card ):
-		if card in self.cards:
-			return self.cards.pop( self.cards.index( card ) )
-
-
-	def check_for_uno( self ):
-	#Checks if a player has 'uno'
-		if len( self.cards ) == 1:
-			self.uno = True
-		  
-		else:
-			self.uno = False
-
-	def is_valid_move( self, card, top_card ):
-
-		#if the played card has the same color as discard pile
-		if card.color == top_card.color:
-		  return True
-		  
-		#if the played card has the same rank as the discard
-		elif card.rank == top_card.rank or card.rank in [ 'w', 'wd4' ]:
-		  return True
-		  
-		else:
-		  return False
-		  
-	def has_valid_move( self, top_card ):
-		for card in self.cards:
-		  if self.is_valid_move( card, top_card ):
-			return True
-		return False
-		  
 class Deck( object ):
-	#19 each of Blue, Green, Red, Yellow (0-9)
-	#8 each Draw Two, Reverse, Skip; 2 of each color
-	#4 Wild cards
-	#4 Wild Draw Four
-	def __init__( self ):
-		self.deck = []
-		for color in COLORS:
-		  for rank in RANKS:
-			if rank == 0:
-			  self.deck.append( Card( color, rank ) )
-			  
-			elif rank in [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ]:
-			  self.deck.append( Card( color, rank ) )
-			  self.deck.append( Card( color, rank ) )
-			  
-			elif rank in [ 'wd4', 'w' ]:
-			  self.deck.append( Card( None, rank ) )
-			  
-			elif rank in [ 's', 'r', 'd2' ]:
-			  self.deck.append( Card( color, rank) )
-			  self.deck.append( Card( color, rank) )
-		  
-	def __str__( self ):
-		deck_rep = " "
-		for card in self.deck:
-			deck_rep += str( card ) + " "
-		return deck_rep
+    '''Jadno deck, made up of Jadno Card objects;
+    19 each of Blue, Green, Red, Yellow (0-9)
+	1 zero, 2 of each number 1-9 per color
+    8 each Draw Two, Reverse, Skip; 2 of each color
+    4 Wild cards
+    4 Wild Draw Four'''
+    def __init__( self, card_list ):
+        self.deck_list = card_list
 
-	def shuffle( self ):
-		random.shuffle( self.deck )
+    def __str__( self ):
+        deck_rep = " "
+        for card in self.deck_list:
+            deck_rep += str( card ) + " "
+        return deck_rep
 
-	def deal_card( self ):
-	  return self.deck.pop()
+    def shuffle( self ):
+        random.shuffle( self.deck_list )
+
+    def deal_card( self ):
+        return self.deck_list.pop()
+			
+def make_cards():
+    cards = []
+    for color in COLORS:
+        for rank in RANKS:
+            if rank in [ 'wd4', 'w' ]:
+                image = 'images/' + rank + '.png'
+                cards.append( Card( None, rank, image ) )
+            else:
+                image = 'images/' + color + rank + '.png'
+                cards.append( Card( color, rank, image ) )
+			
+    return cards
 	
-class Discard( object ):
-	'''The pile where players discard their Cards'''
-
-	def __init__( self ):
-		self.discard = []
-	 
-	def __str__( self ):
-		discard_rep = str( len( self.discard ) ) + ' ' + str( self.discard[ -1 ] )
-		return discard_rep
-
-	def add_card( self, card ):
-		self.discard.append( card )
-	    
-					
+	
