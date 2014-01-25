8cd291fbf9825254fcbdba1c01ea430ccbdcd930
Synergistic <DFernandez226@gmail.com>
2014-01-25 17:55:04 -0500

    Reorganize File Structure
    Moving things around a bit :)

--- main.py
+++ main.py
@@ -1,351 +1,228 @@
-#UNO the game "Jadno"
-import os
-import random
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.scatter import Scatter
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
+from kivy.uix.spinner import Spinner
+from kivy.uix.button import Button
+from kivy.properties import StringProperty, BooleanProperty, ObjectProperty
+from kivy.core.window import Window
 
-RANKS = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 's', 'r', 'd2', 'wd4', 'w' ]
-COLORS = [ 'R', 'G', 'B', 'Y' ]
+from random import randint, choice
+from uno import *
 
-class Card( object ):
-	''' A Card object that represents UNO cards.
-	It  has a rank; 0-9, skip, reverse, draw two,
-	wild, wild draw four. As well as a color; blue, green, red
-	yellow'''
+Window.size = (1280, 720)
 
+player = None
+my_deck = None
+discard_pile = None
+playing = False
+last = False
+color = None
 
-	def __init__( self, color, rank):
-		self.rank = rank
-		self.color = color
-
-	def __str__( self ):
-		if self.color == None:
-		  return str( self.rank )
-		  
-		else:
-		  card_rep = self.color + str( self.rank )
-		  return card_rep
-
-class Hand( object ):
-	'''a Hand is made up of 1+ Uno cards. Cards
-	can be added, or removed (i.e. playing a card)
-	'''
-
-	def __init__( self ):
-		self.cards = []
-		self.uno = False
-		
-	def __str__( self ):
-		hand_rep = " "
-		for card in self.cards:
-			hand_rep += str( card ) + ' '
-		return hand_rep
-
-	def add_card( self, card ):
-		self.cards.append( card )
-
-
-	def discard_card( self, card ):
-		if card in self.cards:
-			return self.cards.pop( self.cards.index( card ) )
-
-
-	def check_for_uno( self ):
-	#Checks if a player has 'uno'
-		if len( self.cards ) == 1:
-			self.uno = True
-		  
-		else:
-			self.uno = False
-
-	def is_valid_move( self, card, top_card ):
-
-		#if the played card has the same color as discard pile
-		if card.color == top_card.color:
-		  return True
-		  
-		#if the played card has the same rank as the discard
-		elif card.rank == top_card.rank or card.rank in [ 'w', 'wd4' ]:
-		  return True
-		  
-		else:
-		  return False
-		  
-	def has_valid_move( self, top_card ):
-		for card in self.cards:
-		  if self.is_valid_move( card, top_card ):
-			return True
-		return False
-		  
-class Deck( object ):
-	'''Jadno deck, made up of Jadno Card objects;
-	19 each of Blue, Green, Red, Yellow (0-9)
-	8 each Draw Two, Reverse, Skip; 2 of each color
-	4 Wild cards
-	4 Wild Draw Four'''
-	def __init__( self ):
-		self.deck = []
-		for color in COLORS:
-		  for rank in RANKS:
-			if rank == 0:
-			  self.deck.append( Card( color, rank ) )
-			  
-			elif rank in [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ]:
-			  self.deck.append( Card( color, rank ) )
-			  self.deck.append( Card( color, rank ) )
-			  
-			elif rank in [ 'wd4', 'w' ]:
-			  self.deck.append( Card( None, rank ) )
-			  
-			elif rank in [ 's', 'r', 'd2' ]:
-			  self.deck.append( Card( color, rank) )
-			  self.deck.append( Card( color, rank) )
-		  
-	def __str__( self ):
-		deck_rep = " "
-		for card in self.deck:
-			deck_rep += str( card ) + " "
-		return deck_rep
-
-	def shuffle( self ):
-		random.shuffle( self.deck )
-
-	def deal_card( self ):
-	  return self.deck.pop()
+class CardImage(Scatter):
+	moving = BooleanProperty(True)
+	src_img = StringProperty(None)
+	card_obj = ObjectProperty(None)
 	
-class Discard( object ):
-	'''The pile where players discard their Cards'''
+	def on_touch_down(self, touch):
+		global last
+		x, y = touch.x, touch.y
 
-	def __init__( self ):
-		self.discard = []
-	 
-	def __str__( self ):
-		discard_rep = str( len( self.discard ) ) + ' ' + str( self.discard[ -1 ] )
-		return discard_rep
-
-	def add_card( self, card ):
-		self.discard.append( card )
-	    
-def player_has_move( player_name, player, top_card ):
-	'''Does the player have any possible moves? If not, then
-	draw a card and check again. If they still don't have any
-	moves after drawing, then skip their current turn.'''
-	
-	global turn
-
-	#if the player has no playable cards
-	if not player.has_valid_move( top_card ):
-
-		
-		new_card_drawn = the_deck.deal_card()
-		print player_name, 'has no valid moves, drawing a card.', new_card_drawn
-		player.add_card( new_card_drawn )
-
-		#if the card is not playable, skip the turn
-		if not player.is_valid_move( new_card_drawn, top_card ): 
-	  
-			print 'Still none, {0}  lose a turn.'.format( player_name )
-			turn += 1
+		# if the touch isnt on the widget we do nothing
+		if not self.collide_point(x, y):
 			return False
-	return True
-  
-def player_discards_card( player, discard_card, top_card ):
-	'''makes sure the players choosen card can legally be discarded.
-	Adds the card to the discard pile and then checks for uno (1 card)'''
-	
-	#check if its a legal move based on current discard card
-	if player.is_valid_move( discard_card, top_card ):
-
-		#move the card from players hand to discard pile
-		discard_pile.add_card( player.discard_card( discard_card ) )
-		
-		player.check_for_uno()
-		
-def match_string_to_card( card_string, player ):
-	''' converts the users entered string to the correct card in hand'''
-
-	if card_string.isalnum():
-	#if the string contains a color
-
-		if card_string[ 0 ] in COLORS:
-			#the color is the first item
-			color = card_string[ 0 ]
-	  
-			# the rank is everything after that
-			rank = card_string[ 1: ]
-
-		#if it does not contain a color
-		elif card_string[ 0: ] in RANKS:
-			color = None
-			rank = card_string[ 0: ]
-
-		else:
-			return False
-	else:
-		return False
-
-  #check for a card with matching color/rank in players hand
-	for card in player.cards:
-		if color == None:
-		  if card.rank == rank:
-			return card
-
-		elif card.rank == rank and card.color == color:
-			return card
-	return False
-
-def game_rules( current_player, cur_string, other_player, oth_string):
-	'''Handles special cards and their functions'''
-	
-	global turn, turn_change, message, playing
-
-	#if it is a skip card, skip the next players turn
-	if choosen_Card.rank == 's':
-		message = '{0} was skipped'.format( oth_string )
-		turn += turn_change * 2
-		
-	#if the card is a wild card	
-	elif choosen_Card.rank in [ 'w', 'wd4' ]:
-	
-		#prompt user for a new color & set the wild card to that color
-		new_color = raw_input( 'Enter the first letter of your desired color: ' ).capitalize()
-		discard_pile.discard[ -1 ].color = new_color
-	
-		turn += turn_change
-		
-		#if its a wd4, make the other player draw cards and skip them
-		if choosen_Card.rank == 'wd4':
-		
-			message = '{0} had the draw four!'.format( oth_string )
-			turn += turn_change
 			
-			for i in range(4):
-				other_player.add_card( the_deck.deal_card() )
-	
-	#if it is a draw 2, make the other player draw and skip them
-	elif choosen_Card.rank == 'd2':
-	
-		message = '{0} had to draw two!'.format( oth_string )
-		turn += turn_change * 2
-		
-		for i in range(2):
-			other_player.add_card( the_deck.deal_card() )				
-	
-	#if its a reverse card, change the direction of turns
-	elif choosen_Card.rank == 'r':
-	
-		message = 'The turn order was reversed'
-		turn_change *= -1
-	
-	#otherwise its a plain number, nothing happens
-	else:
-		turn += turn_change
+		touch.push()
+		touch.apply_transform_2d(self.to_local)
+		touch.pop()
+		print 'touched', str(self.card_obj)
+		last = self.card_obj
+		# if we don't have any active controls, then don't accept the touch
+		if not self.do_translation_x and \
+			not self.do_translation_y and \
+			not self.do_rotation and \
+			not self.do_scale:
+			return False
 
-	#check if the player has uno
-	if current_player.uno:
-		print '''===============
-={0} has Jadno!!=
-==============='''.format( cur_string )
-
-def check_for_win( player, player_string ):
-	'''Check to see if a player has no remaining cards'''
-	
-	if len( player.cards ) == 0:
-		print '{0} Wins!!'.format( player_string )
-		playing = False
+		# grab the touch to track later events
+		self._bring_to_front()
+		touch.grab(self)
+		self._touches.append(touch)
+		self._last_touch_pos[touch] = touch.pos
+		if self.collide_point(450, 400):
+			print "yes"
+			
 		return True
 
-def prompt_user_for_card( player, player_string):
-	'''Display the hand, discard pile, and ask user what they want to play'''
+class GameArea(FloatLayout):
+    pass
 	
-	discard_card = raw_input( 
-	'\n' + player_string + ' Turn\n' + 'Hand: ' + str( player ) + '\nDiscard Pile: ' + str( top_card ) + '\nWhat card would you like to play? ' )
-	  
-	return match_string_to_card( discard_card, player )		
+class HUD(BoxLayout):
+	'''This is what contains all the cards we play with as well as buttons
+	and other interactions with the interface'''
 	
+	card_draw_list = []
+	card_remove_list = []
+	active_card_wids = []
+	discard_wid = None
 	
-#initialize player hands, the deck, and discard pile		
-player1 = Hand()
-player2 = Hand()
-
-the_deck = Deck()
-discard_pile = Discard()
-
-the_deck.shuffle()
-
-#start each player with 7 cards
-for i in range( 7 ):
-	player1.add_card( the_deck.deal_card() )
-	player2.add_card( the_deck.deal_card() )
-
-#pick the first card to start the discard pile
-discard_pile.add_card( the_deck.deal_card() )
-
-#if the first card is a wild, pick a random color for it
-if discard_pile.discard[ -1 ].rank in [ 'w', 'wd4' ]:
-	discard_pile.discard[ -1 ].color = random.choice(COLORS)
-	
-
-	
-#initalize turn clock, turn direction, the message, and start the loop
-turn = 0
-turn_change = 1 #for reverse cards
-playing = True
-
-
-print '''\nWelcome to Jadno, an Uno clone!\n
-Cards are representated as such; COLORcardtype
-For example, a red 2 would be R2.
-===============================\n
-Colors: Red, Yellow, Blue, Green
-w = Wild, wd4 = Wild Draw 4, 
-d2 = Draw 2, r = Reverse, s = Skip\n
-To play a card, simply type in the 
-matching string, it is case sensitive.
-==============================='''
-
-#start game loop
-while playing:
-
-	if turn % 2 == 0: #player 1 turn
-
-	#store the card on top of the discard pile
-		top_card = discard_pile.discard[-1]
-		# os.system('cls' if os.name=='nt' else 'clear')
-		#if the player can make a move
-		if player_has_move( 'Player 1', player1, top_card ):
+	def start_deal(self):
+		'''shuffles, deals seven cards, puts a card on the discard pile,
+		and makes widgets for everything'''
 		
-			choosen_Card = prompt_user_for_card( player1, 'Player 1' )
+		my_deck.shuffle()
+		x = 1 
+		for i in range(7):
+			c = my_deck.deal_card()
+			player.add_card(c)
+			self.make_card_wid(c, (100 * x, 25))
+			x += 1
 			
-			#if its in the players hand
-			if choosen_Card:
+		d =  my_deck.deal_card()
+		if d.rank in ['w', 'wd4']: #if discard starts with wild
+			d.color = choice(COLORS) #we need to give it a random color
+		discard_pile.add_card(d)
+		self.make_card_wid(d, (450, 400), move=False)
 
-				#the player discards a card to the pile
-				player_discards_card( player1, choosen_Card, top_card )
-				
-				#checks for special cards and takes appropriate action
-				game_rules( player1, 'Player 1', player2, 'Player 2')
-				
-				#sees if that was the players last card, for the win
-				if check_for_win( player1, 'Player 1' ):
-					break
-					
-	if turn % 2 == 1: #player 2 turn
-		# os.system('cls' if os.name=='nt' else 'clear')
-		#store the card on top of the discard pile
-		top_card = discard_pile.discard[-1]
+	def start_game(self):
+		'''Initialize deck, discard pile, player hand objects
+		and deal cards, flip the playing variable'''
+		
+		global my_deck, player, discard_pile, playing
+		if playing: #if a game already is happening
+			jadno.clear_gamearea() #clear the board
 
-		#if the player can make a move
-		if player_has_move( 'Player 2', player2, top_card ):
-	  
-			choosen_Card = prompt_user_for_card( player2, 'Player 2')
+		my_deck = Deck(make_cards())
+		discard_pile = Discard()
+		player = Hand()
+		self.start_deal()
+		last = False
+		playing = True
+
+	def make_card_wid(self, card, loc, move=True):
+		'''Takes a card object and makes it into a CardImage widget.
+		The new widget is added to an appropriate list for drawing/tracking.
+		Discard pile will have move=False.'''
+		
+		i = CardImage(src_img = card.image,
+				size = (150, 225),
+				size_hint = (None, None),
+				pos = loc,
+				moving = move,
+				card_obj = card)
+		self.card_draw_list.append(i)
+		self.active_card_wids.append(i)
+		if not move: #store the discard separately for easy identification
+			self.discard_wid = i
+	
+	def add_a_card(self):
+		'''Draws a card from the deck to the player's hand'''
+		
+		if len(my_deck.deck_list) > 0:
+			c = my_deck.deal_card()
+			player.add_card(c)
+			self.make_card_wid(c, (100, 25))
+		else:
+			print "Deck is empty bro"
+
+	def remove_a_card(self):
+		'''Method to remove a card object and it's corresponding widget.'''
+		
+		#make sure user has touched a card
+		if not last:
+			print "No recently touched cards"
 			
-			#if the chosen card is in the player's hand
-			if choosen_Card:
-
-				#the player discards a card to the pile
-				player_discards_card( player2, choosen_Card, top_card )
+		#make sure the last card touched isn't the discard pile
+		elif last == self.discard_wid.card_obj:
+			print "cannot remove discard pile"
 			
-				game_rules( player2, 'Player 2', player1, 'Player 1')
-				
-				if check_for_win( player2, 'Player 2' ):
-					break
-					
+		#make sure the player has cards to discard	
+		elif len(player.hand_list) <= 0:
+			print "You Win"	
+		
+		#check if it's a legal move based on game rules
+		elif is_valid_move(last, self.discard_wid.card_obj):
+			d = player.discard_card(last)
+			self.active_card_wids.remove(self.discard_wid) #current discard widget no longer needed
+			self.card_remove_list.append(self.discard_wid) #to be deleted from parent widget
+			
+			for card in self.active_card_wids:
+				if card.card_obj == d: #remove the played card from the active widgets and parent
+					self.active_card_wids.remove(card)
+					self.card_remove_list.append(card)
+
+		else:
+			print "Not a valid move"
+			
+	def update_discard(self, new_card):
+		'''Method to create a new widget for the discard pile after a player
+		discards a card. Asks user about desired color of the played card is
+		a wild'''
+		
+		#Need to know user's desired new color when playing wild
+		if str(new_card) in ['w', 'wd4']:
+			c = BoxLayout(orientation = 'vertical')
+			c.add_widget(Label(text="Select your new color:"))
+			s = Spinner(values=['r', 'g', 'b', 'y'])
+			c.add_widget(s)
+			b = Button(text='Okay')
+			c.add_widget(b)
+			p = Popup(title="Enter color", size_hint=(0.25, 0.4), content=c, auto_dismiss=False)
+			
+			def close_pop(b):
+				if s.text in ['r', 'g', 'b', 'y']:
+					p.dismiss()
+					new_card.color = s.text
+			b.bind(on_release = close_pop)
+			p.open()
+		
+		#replace the old discard pile widget with a new one
+		self.make_card_wid(new_card, (450, 400), move=False)
+
+#The main window that contains everything
+class JadnoApp(App):
+
+	def build(self):
+		'''Builds a float layout as the base with holds the interface that
+		contains buttons and cards. Handles adding/removing new widgets'''
+		
+		self.root = GameArea()
+		self.h = HUD()
+		self.root.add_widget(self.h)
+		Clock.schedule_interval(self.update, 1.0 / 30.0)
+		return self.root
+		
+	def update(self, dt):
+		'''Handles the adding/removing widgets'''
+		if playing:
+			self.adding_cards()
+			self.removing_cards()
+		
+	def adding_cards(self):
+		for card in self.h.card_draw_list:
+			self.root.add_widget(card)
+		self.h.card_draw_list = []
+			
+	def removing_cards(self):
+		for card in self.h.card_remove_list:
+			self.root.remove_widget(card)
+			#add that card to the discard pile
+			if card != self.h.discard_wid: 
+				self.h.update_discard(card.card_obj)	
+		self.h.card_remove_list = []
+		
+	def clear_gamearea(self):
+		self.root.clear_widgets()
+		self.root.add_widget(self.h)
+
+
+	
+#Start game loop
+jadno = JadnoApp()
+if __name__ == '__main__':
+
+    jadno.run()
+
